5eb6de73cd9466c0ed5e418c65c1c2a9f2b937df
Miguel Angel Alba <miguelangel.abb@gmail.com>
2020-12-29 15:13:31 +0100

    I save the file in a USB folder. I named the file with the time of the beginning of the recording

--- camera.py
+++ camera.py
@@ -2,24 +2,41 @@
 # -*- coding: utf-8 -*-
 
 from picamera import PiCamera
-from time import sleep
+from datetime import datetime, time
+import os
 
 #Const
 
-def rec (camera):
+def timestamp():
+    dt = datetime.now()
+    #Returns a timsStam at the instant it runs in the function.
+    return dt.isoformat(timespec= 'seconds')
+
+def createDirRec(folderName):
+    try:
+        os.mkdir('/media/pi/0113-44041/' + folderName)
+        print ('Creando folder ' + folderName)
+    except OSError as e:
+        print(e)
+
+def rec (camera,folderName,ts):
+    #I hace to format the timestamp becouse Raspivid doesn't accept the ":" in a file name.
+    ts = ts.replace(':','') 
     camera.resolution = (1920, 1080)
     camera.start_preview()
-    camera.start_recording('/media/pi/0113-44041/my_video.h264')
+    camera.start_recording('/media/pi/0113-44041/' + folderName + '/' + ts + '.h264')
     camera.wait_recording(60)
     camera.stop_recording()
 
 
 if __name__ == "__main__":
 
+    folder = 'myVideos'
     camera = PiCamera()
     try:
-        
-        rec(camera)
+        createDirRec(folder)
+        ts = timestamp()
+        rec(camera,folder,ts)
 
     finally:
 
