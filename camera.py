81e987485b3f76b36827f4fdd843ed1390a518c5
Miguel Angel Alba <miguelangel.abb@gmail.com>
2020-12-30 23:02:48 +0100

    night mode configuration

--- camera.py
+++ camera.py
@@ -9,8 +9,7 @@ import os
 
 def timestamp():
     return datetime.now().isoformat(timespec= 'seconds')
-    #Returns a timsStam at the instant it runs in the function.
-    #return dt
+    #Returns a timsStam at the instant it runs in the function in ISO format.
 
 def createDirRec(folderName):
     try:
@@ -23,11 +22,12 @@ def rec (camera,folderName,ts):
     #I hace to format the timestamp becouse Raspivid doesn't accept the ":" in a file name.
     ts = ts.replace(':','') 
     camera.resolution = (1920, 1080)
+    camera.exposure_mode = ('night')
     camera.framerate = (5)
     camera.start_preview()
     camera.annotate_text = timestamp()
     camera.start_recording('/media/pi/0113-44041/' + folderName + '/' + ts + '.h264',sps_timing=True,bitrate=10000000)
-    camera.wait_recording(3600)
+    camera.wait_recording(60)
     camera.stop_recording()
 
 
